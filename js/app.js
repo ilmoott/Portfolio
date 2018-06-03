caa6a8cb326c6f2cbf6b86c8ddbe65429d3b1d95
Ilmo Ott <30363925+ilmoott@users.noreply.github.com>
2018-06-03 22:32:58 +1000

    responsive images, sass variables and animations

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 ;(function(){
 
-    let projects, projectsArray;
+    let projects, projectsArray, name, logo;
 
     projects = document.getElementsByClassName('project');
     projectsArray = Array.from(projects);
@@ -13,6 +13,18 @@
         sr.reveal(e,{duration: 1500})
     });
 
+    let removeName = function (){
+
+        logo = document.getElementById('logo');
+        name = document.getElementById('name');
+        logo.addEventListener('click',function(){
+            logo.style.display = 'none';
+            name.style.display = 'block';
+        })
+    }
+    
+    removeName();
 
 })();
+
 
